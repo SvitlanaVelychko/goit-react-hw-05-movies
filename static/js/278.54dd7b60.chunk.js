e07105461cb00cf92f9846d6994e3308aae7c224
SvitlanaVelychko <96179370+SvitlanaVelychko@users.noreply.github.com>
2022-09-10 06:39:20 +0000

    Deploying to gh-pages from @ SvitlanaVelychko/goit-react-hw-05-movies@0f53128567627821cfb382e2a739dbcbd1d196b7 🚀

---
+++ static/js/278.54dd7b60.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[278],{9278:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Movies"})}}}]);
+//# sourceMappingURL=278.54dd7b60.chunk.js.map
